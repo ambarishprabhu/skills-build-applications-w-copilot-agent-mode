36e0b1e350e65b4a5f4b30e6dff4bdd48e01143f
Ambarish Prabhu <38607522+ambarishprabhu@users.noreply.github.com>
2025-11-11 07:21:08 +0000

    Added missing file

--- octofit-tracker/backend/octofit_tracker/models.py
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -1,25 +1,40 @@
 from djongo import models
 
 class Team(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False)
     name = models.CharField(max_length=100, unique=True)
     universe = models.CharField(max_length=50)
+    class Meta:
+        app_label = 'octofit_tracker'
 
 class User(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False)
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=100)
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    class Meta:
+        app_label = 'octofit_tracker'
 
 class Activity(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     type = models.CharField(max_length=100)
     duration = models.IntegerField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        app_label = 'octofit_tracker'
 
 class Workout(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField()
     date = models.DateField()
+    class Meta:
+        app_label = 'octofit_tracker'
 
 class Leaderboard(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False)
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     points = models.IntegerField()
+    class Meta:
+        app_label = 'octofit_tracker'
